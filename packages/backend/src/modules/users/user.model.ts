7295fa178f850211fc0029a33e6e70c89493530d
Thanuja Ranjeewa <thanuj.tj@gmail.com>
2026-06-06 22:09:15 -0400

    google auth

--- packages/backend/src/modules/users/user.model.ts
+++ packages/backend/src/modules/users/user.model.ts
@@ -105,7 +105,7 @@ UserSchema.index({ googleId: 1 }, { unique: true, sparse: true });
 
 // Hash password before saving
 UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+  if (!this.isModified('password') || !this.password) return next();
   try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
